145511cc1ea3c18518ae95a6a56a041c61ac8670
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-06 16:46:28 +0200

    useState to add an increment and decrement functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
 
 function App() {
+  const [ count, setCount ] = useState(0)
+
+  const changeCount = (increment = true) => {
+    setCount(prevCount => {
+      return( 
+        increment ? prevCount + 1 : prevCount -1
+      );
+    });
+  }
   return (
-    <div>Hello world!</div>
+    <>
+      <p>The count is { count }</p>
+      <button onClick={() => changeCount()}>+1</button>
+      <button onClick={() => changeCount(false)}>-1</button>
+    </>
   );
 }
 
