33737522f524c5242826496014478ee732dcbebb
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-07 15:00:36 +0200

    Working with watchers and APIs withouth easy option to clear efect on unmount

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+}
 
 function App() {
   const [ count, setCount ] = useState(0)
   const [ mousePosition, setMousePosition ] = useState({ x: null, y: null });
   //This sets useState to be a value that comes from external API in this case 'navigator'
   const [status, setStatus ] = useState(navigator.onLine);
-
+  //Using destructuring 
+  const [ { latitude, longitude, speed }, setLocation] = useState(initialLocationState);
+  let mounted = true;
   //Normaly Executed after every render
   useEffect(() => {
-    //console.log("useEffect");
+    //fixes a bug on refresh
+    mounted = true;
     document.title = `Current counter value is ${count}!`;
     window.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('online', handleOnliineStatus);
     window.addEventListener('offline', handleOfflineStatus);
+    //getCurrentPosition does not support an easy way for cleaning on unmount
+    //To ensure that we know when the component is mounted we can set a variable let mounted;
+    navigator.geolocation.getCurrentPosition(handleGeolocation);
+    //Position watcher needs to have a watchID in order to clean it in component unmount
+    const watchID = navigator.geolocation.watchPosition(handleGeolocation);
 
     //This function cleans eventListener on component unmount
     //It is also called every time before the effect runs to cleanup last run
@@ -20,6 +34,8 @@ function App() {
       window.removeEventListener('mousemove', handleMouseMove);
       window.removeEventListener('online', handleOnliineStatus);
       window.removeEventListener('offline', handleOfflineStatus);
+      navigator.geolocation.clearWatch(watchID);
+      mounted = false;
     }
   }, 
 
@@ -41,7 +57,16 @@ function App() {
 
   const handleOnliineStatus = () => setStatus(true);
   const handleOfflineStatus = () => setStatus(false);
- 
+
+  const handleGeolocation = (event) => {
+    console.log(event, mounted)
+    mounted && setLocation({
+      latitude: event.coords.latitude,
+      longitude: event.coords.longitude,
+      speed: event.coords.speed
+    })
+  }
+  
   return (
     <>
       <p>The count is { count }</p>
@@ -55,6 +80,12 @@ function App() {
       <hr />
 
       <p>Your curent status is <strong>{`${status ? "online" : "offline"}`}</strong></p>
+
+      <hr />
+
+      <p>Latitude is { latitude }</p>
+      <p>Longitude is { longitude }</p>
+      <p>Current speed is { speed ? speed : "0" }</p>
     </>
   );
 }
