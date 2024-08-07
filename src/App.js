487112b8c294a48f855a1e74359abe3a5c13d5e3
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-07 13:59:23 +0200

    Working with multiple eventListeners to set state

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import { useState, useEffect } from "react";
 function App() {
   const [ count, setCount ] = useState(0)
   const [ mousePosition, setMousePosition ] = useState({ x: null, y: null });
+  //This sets useState to be a value that comes from external API in this case 'navigator'
+  const [status, setStatus ] = useState(navigator.onLine);
 
   //Normaly Executed after every render
   useEffect(() => {
-    console.log("useEffect");
+    //console.log("useEffect");
     document.title = `Current counter value is ${count}!`;
     window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('online', handleOnliineStatus);
+    window.addEventListener('offline', handleOfflineStatus);
 
     //This function cleans eventListener on component unmount
     //It is also called every time before the effect runs to cleanup last run
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('online', handleOnliineStatus);
+      window.removeEventListener('offline', handleOfflineStatus);
     }
   }, 
 
@@ -33,6 +39,9 @@ function App() {
     setMousePosition({ x: event.pageX, y: event.pageY });
   };
 
+  const handleOnliineStatus = () => setStatus(true);
+  const handleOfflineStatus = () => setStatus(false);
+ 
   return (
     <>
       <p>The count is { count }</p>
@@ -42,6 +51,10 @@ function App() {
       <hr />
 
       <p>Mouse Position: {JSON.stringify(mousePosition, null, 2)}</p>
+
+      <hr />
+
+      <p>Your curent status is <strong>{`${status ? "online" : "offline"}`}</strong></p>
     </>
   );
 }
