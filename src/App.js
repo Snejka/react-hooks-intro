4bbd67639a8c881dbc33b9e69039767fbfd7299b
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-08 14:17:49 +0200

    Creating a search box for dynamic query

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import axios from "axios";
 export default function App() {
 
   const [ results, setResults ] = useState([]);
+  const [ query, setQuery ] = useState('reacthooks')
 
   useEffect(() => {
     getResults();
-  }, []);
+  }, [query]);
 
   const getResults = async () =>{
-    const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks');
+    const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
     setResults(response.data.hits);
   }
 
   return (
-    <ul>
-      {results.map(result => (
-        <li key={result.objectID}>
-          <a href={result.url}>{result.title}</a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <input type="search" onChange={(e)=>{setQuery(e.target.value)}}/>
+      {(results.length !== 0) && <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>}
+    </>
   );
 }
