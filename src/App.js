9503d36d1994d3e97b05e4184ec7b4ddab91b0ff
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-09 16:22:13 +0200

    Using useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React, { useContext } from "react";
-import { UserContext } from "./index";
+import React, { useContext, useReducer } from "react";
+// import { UserContext } from "./index";
+
+const initialState = {
+  count: 0
+};
+
+function reducer(state, action) {
+
+  switch(action.type) {
+    case "increment": 
+      return {
+        count: state.count + 1
+      };
+      case "decrement": 
+      return {
+        count: state.count - 1
+      };
+    default:
+      return initialState
+  }
+
+}
 
 export default function App() {
-  const value = useContext(UserContext);
-  
+
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+  // const value = useContext(UserContext);
+
   return (
     <div>
-      <p> Hello, {value}!</p>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({type: "increment"})}>+</button>
+      <button onClick={() => dispatch({type: "decrement"})}>-</button>
+      <button onClick={() => dispatch({type: "reset"})}>Reset</button>
     </div>
   );
 }
