c3c5b6125c7895977bb647306061e2c40d41f992
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-14 16:23:28 +0200

    Combine useState and useReducer to make initial app state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React, { createContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const username = "Dave";
-export const UserContext = createContext(username);
+import TodosContext from './context';
+import todosReducer from './reducer';
+
+import TodoList from './components/TodoList';
+
+const App = () => {
+  const initialState = useContext(TodosContext);
+  const [ state, dispatch ] = useReducer(todosReducer, initialState);
+
+  return (
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <TodoList />
+    </TodosContext.Provider>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserContext.Provider value={username}>
-      <App /> 
-    </UserContext.Provider>
+    <App />
   </React.StrictMode>
 );
 
