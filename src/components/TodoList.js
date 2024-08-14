8ffa6e77b08692871208b6d084593c9998655468
Snezhana Krasteva <snezhana.p.plachkova@gmail.com>
2024-08-14 17:15:11 +0200

    Add Toggle Todos Action

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,13 +4,21 @@ import TodosContext from "../context";
 export default function TodoList () {
   const { state, dispatch } = useContext(TodosContext);
 
+  const title = (state.todos.length > 0) ? `${state.todos.length} Todos!` : "Nothing To Do!";
+
   return (
-    <ul>
-        {state.todos.map( todo => (
-            <li key={todo.id}>
-                {todo.text}
-            </li>)
-        )}
-    </ul>
+    <>
+        <h1>{title}</h1>
+        <ul>
+            {state.todos.map( todo => (
+                <li key={todo.id} 
+                    onDoubleClick={() => dispatch({ type: "TOGGLE_TODO", payload: todo })}
+                    style={{ color: todo.complete ? 'yellow' : 'blue' }}
+                >
+                    {todo.text + " " +( todo.complete ? "Done!" : "")}
+                </li>)
+            )}
+        </ul>
+    </>
   )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,13 @@
-
+const actionTodosToggle = (state, payload) => state.todos.map( todo => (todo.id === payload.id) ? {...payload, complete: !payload.complete } : todo );
 
 export default function reducer(state, action) {
     switch (action.type){
+        case "TOGGLE_TODO":
+            const toggledTodos = actionTodosToggle(state, action.payload);
+            return {
+                ...state,
+                todos: toggledTodos
+            }
         default:
             return state;
     }
